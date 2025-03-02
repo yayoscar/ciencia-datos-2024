a377efb4dcc4d5ec4553c088e09c44a140fe4b4f
Ariathna Cedaces <ariathna.cedacesgomez@cbtis72.edu.mx>
2025-03-02 14:53:11 -0600

    ejercicios1_2

---
+++ ejercicio01/tareas/for_contar_letras.py
@@ -0,0 +1,4 @@
+palabras = ["sol", "elefante", "rojo", "mariposa", "casa", "extraterrestre"]
+
+for palabra in palabras:
+    print(f"La palabra '{palabra}' tiene {len(palabra)} letras.")
---
+++ ejercicio01/tareas/formato_nombre.py
@@ -0,0 +1,7 @@
+
+nombre_apellido = input("Por favor, ingresa tu nombre y apellido en minúsculas: ")
+
+nombre_formateado = nombre_apellido.title()
+
+print("El nombre correctamente formateado es:")
+print(nombre_formateado)
