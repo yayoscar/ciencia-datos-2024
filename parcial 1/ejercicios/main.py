e4109dd933320257df914b88cbc183c6f45328f1
Oscar Pérez <operez@customsoft.biz>
2025-04-06 21:51:05 -0500

    test

--- parcial 1/ejercicios/main.py
+++ parcial 1/ejercicios/main.py
@@ -1,7 +1,7 @@
 mensaje = "Ingrese una tarea: "
 todos = []
 while True:
-    accion_usuario = input("Indica que acción quieres realizar agregar/mostrar/editar/completar/salir: ")
+    accion_usuario = input("Indica que acción quieres realizar agregar/mostrar/salir: ")
     accion_usuario = accion_usuario.lower()
     accion_usuario = accion_usuario.strip()
     match accion_usuario:
@@ -9,17 +9,9 @@ while True:
             todo = input(mensaje)
             todos.append(todo)
         case "mostrar"| "listar":
-            for indice,elemento in enumerate(todos):
+            for elemento in todos:
                 elemento = elemento.title()
-                print(f"{indice+1}--{elemento}")
-        case "editar":
-            indice= int(input("Ingrese el número de la lista a editar: "))
-            nueva_tarea = input("Ingrese el nuevo valor para la tarea: ")
-            todos[indice-1] = nueva_tarea
-        case "completar":
-            indice = int(input("Ingrese el número de la lista a completar: "))
-            indice -= 1
-            todos.pop(indice)
+                print(elemento)
         case "salir":
             break
         case _:
