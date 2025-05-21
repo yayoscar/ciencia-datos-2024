15a594ca7d4a6a56ae30d1ea6ff78a2daaef8e97
SamNbc <samir.balamcaamal@cbtis72.edu.mx>
2025-05-21 09:11:38 -0500

    examen

--- Parcial-2/Tareas/bloque_1/buscar_palabra_en_archivo.py
+++ Parcial-2/Tareas/bloque_1/buscar_palabra_en_archivo.py
@@ -3,5 +3,4 @@ palabra = input("Escribe la palabra que deseas buscar: ")
 with open("texto.txt", "r") as archivo:
     contenido = archivo.read()
     cantidad = contenido.lower().count(palabra.lower())
-
 print(f"La palabra aparece {cantidad} veces.")
