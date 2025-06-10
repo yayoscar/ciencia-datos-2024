bc6a87a4bf54ff2e08e4bb7a4cce902d5fe2f7a2
Yulitza Esmeralda Alcocer Quijada <yulitza.alcocerquiajada@cbtis72.edu.mx>
2025-06-10 13:30:43 -0500

    avance-2

--- pec/main.py
+++ pec/main.py
@@ -1,24 +1,29 @@
 import FreeSimpleGUI as sg
 import csv
+
 layout = [
- [sg.Text("Monto del gasto:"), sg.Input(key="MONTO")],
- [sg.Text("Categoría:"), sg.Combo(["Comida", "Transporte", "Otros"],
-key="CATEGORIA")],
- [sg.Text("Fecha (opcional):"), sg.Input(key="FECHA")],
- [sg.Button("Guardar gasto"), sg.Button("Ver resumen")]
- ]
-window = sg.Window("Registro de Gastos y Categorías",layout,font=('Arial',25))
-window.read()
-
-datos = [
-    ["MONTO"],
-    ["CATEGORIA"],
-    ['FECHA']
-
+    [sg.Text("Monto del gasto:"), sg.Input(key="MONTO")],
+    [sg.Text("Categoría:"), sg.Combo(["Comida", "Transporte", "Otros"], key="CATEGORIA")],
+    [sg.Text("Fecha (opcional):"), sg.Input(key="FECHA")],
+    [sg.Button("Guardar gasto"), sg.Button("Ver resumen")]
 ]
 
+window = sg.Window("Registro de Gastos y Categorías", layout, font=('Arial', 25))
 
-with open("datos.csv","w") as archivo:
-          writer = csv.writer(archivo)
-          writer.writerows(datos)
+while True:
+    evento, values = window.read()
+    if evento == sg.WIN_CLOSED:
+        break
+    elif evento == "Guardar gasto":
+        Monto = values["MONTO"]
+        Categoria = values["CATEGORIA"]
+        Fecha = values["FECHA"]
+
+        with open("datos.csv", "a", newline='') as archivo:
+            writer = csv.writer(archivo)
+            writer.writerow([f"El monto es de: {Monto}"])
+            writer.writerow([f"La categoría es: {Categoria}"])
+            writer.writerow([f"La fecha es: {Fecha}"])
+
+window.close()
 
