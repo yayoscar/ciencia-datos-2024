051df263140c4f16792d17bcf8bcf9b548e21e51
Julio Saens <julioyaelhuchinsaenz@gmail.com>
2025-06-10 02:30:34 -0500

    avance n2

--- pec/funciones.py
+++ pec/funciones.py
@@ -1,5 +1,13 @@
 import FreeSimpleGUI as sg
+import csv
+import os
 
-layout = [
-    [sg.Input(key="numero1"),sg.Text("+"),sg.Input(key="numero2"),sg.Text("=???")],
-    [sg.Button("Sumar")]
+def guardar_gastos(ventana, fecha, monto, categoria):
+    write_header = not os.path.exists('datos.csv')
+    with open('datos.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        if write_header:
+            writer.writerow(['fecha', 'monto', 'categoria'])
+        writer.writerow([fecha, monto, categoria])
+    ventana['-STATUS-'].update('Datos guardados correctamente.')
+    return
--- pec/main.py
+++ pec/main.py
@@ -1,14 +1,28 @@
 import FreeSimpleGUI as sg
+import csv
+import os
+import funciones
+from pec.funciones import guardar_gastos
 
 layout= [
     [sg.Text("Monto del gasto: "), sg.Input(key="MONTO")],
     [sg.Text("Categoria"), sg.Combo(["Comida", "Transporte", "Otros"], key="CATEGORIA")],
     [sg.Text("Fecha (opcional)"), sg.Input(key="FECHA")],
-    [sg.Button("Guardar gasto"), sg.Button("Ver resumen")]
+    [sg.Button("Guardar gasto"), sg.Button("Ver resumen")],
+    [sg.Text('', size=(40,1), key='-STATUS-')]
 
 ]
 
 ventana = sg.Window("Registro de Gastos y Categorias",layout,font=("Comic Sans",20))
 
-ventana.read()
+while True:
+    event, values = ventana.read()
+    if event in (sg.WIN_CLOSED, 'Salir'):
+        break
+    elif event == 'Guardar gasto':
+        monto= values["MONTO"]
+        categoria= values["CATEGORIA"]
+        fecha= values["FECHA"]
+        guardar_gastos(ventana, fecha, monto, categoria)
+
 ventana.close()
