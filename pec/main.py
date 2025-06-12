2d0f135494aa65f732b731b7523919e05715f126
Karen Tut <karen.tutcabrera@cbtis72.edu.mx>
2025-06-12 12:16:20 -0500

    Avances

--- pec/main.py
+++ pec/main.py
@@ -1,15 +1,33 @@
 import FreeSimpleGUI as sg
 
-layout=[
-    [sg.Text("Nombre del gasto hormiga:"), sg.Input(key="NOMBRE")],
-    [sg.Text("Precio por unidad"), sg.Input(key="PRECIO")],
-    [sg.Text("Veces por semana:"), sg.Input(key="VECES")],
-    [sg.Text("Meses:"), sg.Input(key="VECES")],
-    [sg.Button("Calcular ahorro")]
 
+layout=[
+    [sg.Text("Nombre del gasto hormiga (ej.Cafe):",size=(20, 1)), sg.Input(key="NOMBRE")],
+    [sg.Text("Precio por unidad",size=(20, 1)), sg.Input(key="PRECIO")],
+    [sg.Text("Veces por semana:",size=(20,1)), sg.Input(key="VECES")],
+    [sg.Text("Meses:",size=(20, 1)), sg.Input(key="VECES")],
+    [sg.Button("Calcular ahorro"), sg.Button("Salir")],
+    [sg.Text(size=(40, 2), key="RESULTADO")]
 
 ]
 
-Window = sg.Window("Proyecto 3: Calcular ahorro",layout,font=("Arial, 12"))
-Window.read()
+ventana = sg.Window("Proyecto 3: Calcular ahorro",layout,font=("Arial", 20))
+while True:
+   evento, valores = Window.read()
+    if evento ==sg.WIN_CLOSED or evento == "Salir":
+        break
+
+    if evento == "Calcular ahorro":
+        try:
+            nombre = valores["GASTO HORMIGA"]
+            precio = valores["PRECIO POR UNI"]
+            veces= valores["VECES POR SEMANA"]
+            meses = valores["MESES"]
+
+            gasto_total = precio * veces * 4 * meses
+            ventana["RESULTADO"]
+
+
+
+
 Window.close()
