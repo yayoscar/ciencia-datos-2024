8bc7bd1dee3e1bbe5158ff8003c9046402156725
Yigael Cohuo <yigael.cohuokumul@cbtis72.edu.mx>
2025-06-24 21:09:07 -0500

    layout mejorado.

--- ciencia_datos/pec/main.py
+++ ciencia_datos/pec/main.py
@@ -3,10 +3,11 @@ import FreeSimpleGUI as sg
 
 
 layout = [
- [sg.Text("META DE AHORRO:"), sg.Input(key="META")],
- [sg.Text("NUMERO DE SEMANAS:"), sg.Input(key="SEMANAS")],
- [sg.Text("APORTE ACTUAL:"), sg.Input(key="APORTE")],
- [sg.Button("REGISTRAR APORTE"), sg.Button("VER PROGRESO")]
+ [sg.Text("META DE AHORRO:", size=(20, 1), text_color="black"), sg.Input(key="META")],
+ [sg.Text("NUMERO DE SEMANAS:", size=(20, 1), text_color="black"), sg.Input(key="SEMANAS")],
+ [sg.Text("APORTE ACTUAL:", size=(20, 1), text_color="black"), sg.Input(key="APORTE")],
+    [sg.Button("REGISTRAR APORTE", button_color=("white", "#28a745")), sg.Button("VER PROGRESO", button_color=("white", "#007bff"))],
+
  ]
 window= sg.Window("Reto de Ahorro Personalizado",layout,font=("arial",20))
 
